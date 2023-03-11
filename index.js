93403dd595e759f4a9634eb21c0a0a6510738b26
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-03-11 16:48:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  let salary = 0;
+  let capital = 0;
+  let installment = 0;
+  let n = 0; //number of monthly installments
+  let interestRate = 0.08; ///fixed interest rate
+  let totalInterest = 0;
+  
+  readline.question('Enter your monthly salary: ', (salaryInput) => {
+    salary = parseFloat(salaryInput);
+    installment = 0.2 * salary;  //the installment can only be 20% of the salary
+    readline.question('Enter the number of monthly installments you require: ', (nInput) => {
+      n = parseInt(nInput);
+      capital = installment * ((1-(1/Math.pow(1+interestRate, n)))/interestRate);
+      totalInterest = (installment * n) - capital;
+      capital = capital.toFixed(2);
+      installment = installment.toFixed(2);
+      totalInterest = totalInterest.toFixed(2);
+      console.log('Under French amortization system:\nCapital: ' + capital + '\nInstallment: ' + installment + '\nTotal interest: ' + totalInterest + '\nInterest rate: ' + interestRate*100+'%');
+      readline.close();
+    });
+  });
+  
